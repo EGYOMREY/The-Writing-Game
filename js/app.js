7db42a190b030f53d8374eee0763d5b195f8baab
Eva Gyomrey <evamargy@gmail.com>
2017-12-02 15:17:04 -0400

    - Add more words to the easyArray - Everytime a button is clicked, the button's id is saved so the input can know which difficulty the user is playing and can reset the timer properly - Add resetCountdown functions for each difficulty (easy, medium, hard, mix), so each have different limit times - Add theCountdown function which works as a timer, it's executed every second while the user doesn't put any input - The input event listener was modified so it can restart the countdown depending on the difficulty

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 //Our data
 var words = [];
 
-var easyArray = ["clerk", "log", "will", "inn", "acid", "child", "field", "jam", "blow", "cool", "tank", "chop", "folk", "plan", "bank", "frog", "poll", "rich", "toll", "home", "call", "toss"];
+var easyArray = ["clerk", "log", "will", "inn", "acid", "child", "field", "jam", "blow", "cool", "tank", "chop", "folk", "plan", "bank", "frog", "poll", "rich", "toll", "home", "call", "toss", "long", "cart", "card", "yell", "real", "pork", "salt", "lace", "rung", "pier", "meat", "pace", "belt", "mail", "rate", "flag", "menu", "roll", "case", "star", "warn", "bean", "lean", "sale", "lift", "fork", "cane", "bear", "gaffe", "alarm", "handy", "close", "grace", "ivory", "pluck", "plane", "forge", "beard", "stick", "exile", "shark"];
+
 
 var mediumArray = ["export", "helmet", "notice", "tactic", "pepper", "review", "waiter", "senior", "reform", "stream", "redeem", "grudge", "credit", "absent", "bottom", "hiccup", "dignity", "discuss", "welfare", "trouble", "bedroom", "cabinet", "bedroom", "dismiss", "comment", "extract", "realize", "mystery", "outline", "radical", "applaud", "equinox", "genuine", "auction", "graphic", "limited", "recruit", "publish"];
 
@@ -20,60 +21,56 @@ Array.prototype.randomElement = function() {
 // select the elements of the DOM we are going to use.
 var displayWord = document.querySelector('.displayWord');
 var input = document.querySelector('#input');
-var easy = document.querySelector("#easy");
-var medium = document.querySelector("#medium");
-var hard = document.querySelector("#hard");
-var mix = document.querySelector('#mix');
+var easyMode = document.querySelector("#easyMode");
+var mediumMode = document.querySelector("#mediumMode");
+var hardMode = document.querySelector("#hardMode");
+var mixMode = document.querySelector('#mixMode');
 var finalPoints = document.querySelector('.finalPoints');
+var timer = document.querySelector('#timer');
 
 var randomWord;
 var userPoints;
+var timerNumber;
+var intervalID;
+var buttonClicked;
 
 // Add event listeners to each button.
-easy.addEventListener('click', function() {
+easyMode.addEventListener('click', function(e) {
+    buttonClicked = e.target.id;
     reset();
-    (function delayedAlert() {
-        setTimeout(function slowAlert() {
-            input.disabled = true;
-            finalPoints.innerHTML = "Final Points: " + userPoints;
-        }, 6000);
-    })();
+    easyResetCountdown();
+    input.focus();
+    intervalID = setInterval(theCountdown, 600);
     words = easyArray.slice();
     randomize();
 });
 
-medium.addEventListener('click', function() {
+mediumMode.addEventListener('click', function(e) {
+    buttonClicked = e.target.id;
     reset();
-    (function delayedAlert() {
-        setTimeout(function slowAlert() {
-            input.disabled = true;
-            finalPoints.innerHTML = "Final Points: " + userPoints;
-        }, 6000);
-    })();
+    mediumResetCountdown();
+    input.focus();
+    intervalID = setInterval(theCountdown, 600);
     words = mediumArray.slice();
     randomize();
 });
 
-hard.addEventListener('click', function() {
+hardMode.addEventListener('click', function(e) {
+    buttonClicked = e.target.id;
     reset();
-    (function delayedAlert() {
-        setTimeout(function slowAlert() {
-            input.disabled = true;
-            finalPoints.innerHTML = "Final Points: " + userPoints;
-        }, 6000);
-    })();
+    hardResetCountdown();
+    input.focus();
+    intervalID = setInterval(theCountdown, 600);
     words = hardArray.slice();
     randomize();
 });
 
-mix.addEventListener('click', function() {
+mixMode.addEventListener('click', function(e) {
+    buttonClicked = e.target.id;
     reset();
-    (function delayedAlert() {
-        setTimeout(function slowAlert() {
-            input.disabled = true;
-            finalPoints.innerHTML = "Final Points: " + userPoints;
-        }, 10000);
-    })();
+    mixResetCountdown();
+    input.focus();
+    intervalID = setInterval(theCountdown, 600);
     words = allArrays.slice();
     randomize();
 });
@@ -81,10 +78,21 @@ mix.addEventListener('click', function() {
 // If the word the user types in the input field is the 
 // same as the random word, the user gets a point and
 // a new word is generated
-input.addEventListener("input", function() {
+input.addEventListener("input", function(e) {
     var num = this.value;
     if (num == randomWord) {
         userPoints++;
+        if (buttonClicked == 'easyMode') {
+            easyResetCountdown();
+        } else if (buttonClicked == 'mediumMode') {
+            mediumResetCountdown();
+        } else if (buttonClicked == 'hardMode') {
+            hardResetCountdown();
+        } else if (buttonClicked == 'mixMode') {
+            mixResetCountdown();
+        }
+        clearInterval(intervalID);
+        intervalID = setInterval(theCountdown, 600);
         input.value = '';
         randomize();
     }
@@ -92,14 +100,65 @@ input.addEventListener("input", function() {
 
 // Sets the default initial values
 function reset () {
+    clearInterval(intervalID);
     input.value = '';
     input.disabled = false;
     userPoints = 0;
     finalPoints.innerHTML = '';
+    timer.style.display = "block";
 }
 
 // Generates a new random word and writes it on the DOM
 function randomize () {
     randomWord = words.randomElement();
     displayWord.innerHTML = randomWord;
+}
+
+/*
+This functions resets the timer that is displayed to
+the user depending on the difficulty*/
+function easyResetCountdown() {
+    console.log('EASY!');
+    timerNumber = 2;
+    timer.innerHTML = timerNumber;
+
+}
+
+function mediumResetCountdown() {
+    console.log('MEDIUM!');
+    timerNumber = 3;
+    timer.innerHTML = timerNumber;
+
+}
+
+function hardResetCountdown() {
+    console.log('HARD!');
+    timerNumber = 4;
+    timer.innerHTML = timerNumber;
+
+}
+
+function mixResetCountdown() {
+    console.log('MIX!');
+    timerNumber = 4;
+    timer.innerHTML = timerNumber;
+
+}
+
+/* TIMER FUNCTION */
+// this function works as a timer, every time it's executed, 
+// the timerNumber is evaluated, if it hit zero(0), the timer
+// will hide and reset. The final points are going to get displayed
+// to the user. Else: it substracts the timerNumber by 1 and displayed
+// to ther user
+function theCountdown() {
+    if (timerNumber == 1) {
+        timer.style.display = "none";
+        input.disabled = true;
+        finalPoints.innerHTML = "Final Points: " + userPoints;
+        clearInterval(intervalID);
+    } else {
+        timerNumber--;
+        timer.innerHTML = timerNumber;
+    }
 }
